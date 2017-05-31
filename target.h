d629e3f323c66de51bcfe26ed057a17cb61416f2
Jesko <jesko@huettenhain.net>
2017-05-31 16:19:01 +0200

    working version with macros

--- target.h
+++ target.h
@@ -6,8 +6,11 @@
 #define Subscript(a,i) a[i]
 #define Power field_pow
 
-static const int DEGREE;
-static const int DIMENSION;
+//static const int DEGREE;
+//static const int DIMENSION;
+
+#define DEGREE 3
+#define DIMENSION 9
 
 void __print_constant(FILE* f, char c);
 void __print_variable(FILE* f, int v);
